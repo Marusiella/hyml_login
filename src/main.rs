f958bb2458f9b26a18499be8a714e08c69c0b6c5
mark <67511608+Marusiella@users.noreply.github.com>
2021-10-01 18:14:07 +0200

    dodanie logowania zlego hasla i wiele wiecej

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ struct Register {
 }
 
 #[post("/login")]
-async fn login(req_body: String) -> impl Responder {
+async fn login(req_body: String, session: Session) -> impl Responder {
     // session: Session
     let v: Login = match serde_qs::from_str(&req_body) {
         Ok(v) => v,
@@ -32,41 +32,77 @@ async fn login(req_body: String) -> impl Responder {
     // } else {
     //     session.set("counter", 1).unwrap();
     // }
-
+    let client = Client::with_uri_str("mongodb://localhost:27017").unwrap();
+    let database = client.database("user");
+    let collection = database.collection::<Register>("users");
+    let x = collection
+        .find_one(doc! {"email":&v.email,"password":&v.password}, None)
+        .unwrap();
+    
+    if x.is_none() {
+        return HttpResponse::Ok().body(format!(r#"<script type="text/javascript">
+        window.location.href = "{a}"
+    </script>         If you are not redirected automatically, follow this <a href='{a}'>link to example</a>.
+    "#,a="/login_f_wrong_passwd.html"));
+    }
     HttpResponse::Ok().body(format!(
-        "email: {} \npassword: {}",
+        "email: {} \npassword: {}\nyour sex: {:?}",
         decode(&v.email).unwrap().to_string(),
-        v.password
+        v.password,
+        x.unwrap_or_else(|| {
+            Register {
+            email: "String".to_owned(),
+            password: "String".to_owned(),
+            dwa: "String".to_owned(),
+            sex: "String".to_owned(),
+        }})
     ))
 }
 
 #[post("/register")]
 async fn register(req_body: String) -> impl Responder {
-    println!("{}", req_body);
     let v: Register = match serde_qs::from_str(&req_body) {
         Ok(v) => v,
         Err(_) => {
             serde_qs::from_str("email=none%40none.none&password=none&dwa=none&sex=none").unwrap()
         }
     };
-    if v.password == v.dwa {
-        let client = Client::with_uri_str("mongodb://localhost:27017").unwrap();
-        let database = client.database("user");
-        let collection = database.collection::<&Register>("users");
-        collection.insert_one(&v, None).unwrap();
-        return HttpResponse::Ok().body(format!(
-            "email: {} \npassword: {}\ndwa: {}\nsex: {}",
-            decode(&v.email).unwrap().to_string(),
-            v.password,
-            v.dwa,
-            v.sex
-        ));
+    let client = Client::with_uri_str("mongodb://localhost:27017").unwrap();
+    let database = client.database("user");
+    let collection = database.collection::<Register>("users");
+    let x = collection
+        .find_one(doc! {"email":&v.email}, None)
+        .unwrap();
+    if !x.is_some() {
+        if v.password == v.dwa {
+            let client = Client::with_uri_str("mongodb://localhost:27017").unwrap();
+            let database = client.database("user");
+            let collection = database.collection::<&Register>("users");
+            collection.insert_one(&v, None).unwrap();
+            // return HttpResponse::Ok().body(format!(
+            //     "email: {} \npassword: {}\ndwa: {}\nsex: {}",
+            //     decode(&v.email).unwrap().to_string(),
+            //     v.password,
+            //     v.dwa,
+            //     v.sex
+            // ));
+            return HttpResponse::Ok().body(format!(r#"<script type="text/javascript">
+            window.location.href = "{a}"
+        </script>         If you are not redirected automatically, follow this <a href='{a}'>link to example</a>.
+        "#,a="/login.html"));
+        } else {
+            return HttpResponse::Ok().body(format!(r#"<script type="text/javascript">
+            window.location.href = "{a}"
+        </script>         If you are not redirected automatically, follow this <a href='{a}'>link to example</a>.
+        "#,a="/register_f_not_same_passwd.html"));
+        }
     } else {
         return HttpResponse::Ok().body(format!(r#"<script type="text/javascript">
-        window.location.href = "{a}"
-    </script>         If you are not redirected automatically, follow this <a href='{a}'>link to example</a>.
-    "#,a="/register_f_not_same_passwd.html"));
+            window.location.href = "{a}"
+        </script>         If you are not redirected automatically, follow this <a href='{a}'>link to example</a>.
+        "#,a="/register_user_exist.html"));
     }
+    
 }
 
 #[actix_web::main]
