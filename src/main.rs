b6e9c4275bec45c309c84c70cef5ae34e5df97ea
mark <67511608+Marusiella@users.noreply.github.com>
2021-10-01 16:00:00 +0200

    html dziala

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,41 @@
 use actix_web::{get, web};
 use actix_web::{post, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use serde::{Serialize, Deserialize};
+use urlencoding::decode;
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct QueryParams {
+struct Login {
     email:String,
     password:String,
 }
 
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+struct Register {
+    email:String,
+    password:String,
+    dwa: String,
+    sex: String
+}
+
+
 #[post("/login")]
 async fn login(req_body: String) -> impl Responder {
     println!("{}", req_body);
-    let v:QueryParams = serde_qs::from_str(&req_body).unwrap();
-    HttpResponse::Ok().body(format!("email: {} \npassword: {}", v.email, v.password))
+    let v:Login = serde_qs::from_str(&req_body).unwrap();
+    HttpResponse::Ok().body(format!("email: {} \npassword: {}", decode(&v.email).unwrap().to_string(), v.password))
 }
+
+#[post("/register")]
+async fn register(req_body: String) -> impl Responder {
+    println!("{}", req_body);
+    HttpResponse::Ok()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(web::scope("/api").service(login))
+            .service(web::scope("/api").service(login).service(register))
             .service(
                 actix_files::Files::new("/", "./html")
                     .show_files_listing()
