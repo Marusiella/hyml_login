0c9999a4009f7c099b399bed89c81aa227c2a394
mark <67511608+Marusiella@users.noreply.github.com>
2021-10-01 16:38:35 +0200

    zapisanie

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,66 @@
-use actix_web::{get, web};
-use actix_web::{post, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use serde::{Serialize, Deserialize};
+use actix_web::web;
+use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 use urlencoding::decode;
+use actix_session::{Session, CookieSession};
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 struct Login {
-    email:String,
-    password:String,
+    email: String,
+    password: String,
 }
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 struct Register {
-    email:String,
-    password:String,
+    email: String,
+    password: String,
     dwa: String,
-    sex: String
+    sex: String,
 }
 
-
 #[post("/login")]
-async fn login(req_body: String) -> impl Responder {
-    println!("{}", req_body);
-    let v:Login = serde_qs::from_str(&req_body).unwrap();
-    HttpResponse::Ok().body(format!("email: {} \npassword: {}", decode(&v.email).unwrap().to_string(), v.password))
+async fn login(req_body: String) -> impl Responder { // session: Session
+    let v: Login = match serde_qs::from_str(&req_body) {
+        Ok(v) => v,
+        Err(_) => return HttpResponse::Ok().body("coś tam kombinujesz  ;("),
+    };
+    // if let Some(count) = session.get::<i32>("counter").unwrap() {
+    //     println!("SESSION value: {}", count);
+    //     session.set("counter", count + 1).unwrap();
+    // } else {
+    //     session.set("counter", 1).unwrap();
+    // }
+        
+    HttpResponse::Ok().body(format!(
+        "email: {} \npassword: {}",
+        decode(&v.email).unwrap().to_string(),
+        v.password
+    ))
 }
 
 #[post("/register")]
 async fn register(req_body: String) -> impl Responder {
     println!("{}", req_body);
-    HttpResponse::Ok()
+    let v: Register = match serde_qs::from_str(&req_body) {
+        Ok(v) => v,
+        Err(_) => {
+            serde_qs::from_str("email=none%40none.none&password=none&dwa=none&sex=none").unwrap()
+        }
+    };
+    if v.password == v.dwa {
+        HttpResponse::Ok().body(format!(
+            "email: {} \npassword: {}\ndwa: {}\nsex: {}",
+            decode(&v.email).unwrap().to_string(),
+            v.password,
+            v.dwa,
+            v.sex
+        ))
+    } else {
+        HttpResponse::Ok().body(format!(r#"<script type="text/javascript">
+        window.location.href = "{a}"
+    </script>         If you are not redirected automatically, follow this <a href='{a}'>link to example</a>.
+    "#,a="/register_f_not_same_passwd.html"))
+    }
 }
 
 #[actix_web::main]
